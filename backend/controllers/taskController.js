3d3066ff73f27c0be9b4478dbe0799c47074e20d
Le Trung Thanh <tle12340987@gmail.com>
2026-06-30 22:16:33 +0700

    bổ sung API XÓA VÀ CẬP NHẬT

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -18,7 +18,31 @@ const createTask = async (req, res) => {
         res.status(500).json({ message: 'không lưu được dữ liệu', error })
     }
 }
+
+const updateTask = async (req, res) => {
+    try {
+        const { id } = req.params
+        const { title, isComplete } = req.body
+        const updateTask = await Task.findByIdAndUpdate(id, { title, isComplete }, { new: true })
+        res.status(200).json({ message: 'cập nhật thành công', updateTask })
+    } catch (error) {
+        res.status(500).json({ message: 'đã xảy ra lỗi', error })
+    }
+}
+
+const deleteTask = async (req, res) => {
+    try {
+        const { id } = req.params
+        const deleteTask = await Task.findByIdAndDelete(id)
+        res.status(200).json({ message: 'đã xóa task:', id })
+    } catch (error) {
+        res.status(500).json({ message: 'có lỗi xảy ra', error })
+    }
+}
+
 module.exports = {
     getTask,
-    createTask
+    createTask,
+    updateTask,
+    deleteTask
 }
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const { getTask, createTask } = require('../controllers/taskController')
+const { getTask, createTask, updateTask, deleteTask } = require('../controllers/taskController')
 router.get('/', getTask)
 router.post('/', createTask)
+router.put('/:id', updateTask)
+router.delete('/:id', deleteTask)
+
 module.exports = router
